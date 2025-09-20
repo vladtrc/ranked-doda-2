66faef92ba77326504f566da80f6a1914649d10c
vtroc7 <vtroc7@gmail.com>
2025-09-20 12:27:06 +0300

    rm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from data_to_duckdb import load_matches_into_duckdb
-from parse_data import parse_dota_file, Match
-from dataclasses import asdict
-from typing import List
-import duckdb
+from parse_data import parse_dota_file
 
 matches = parse_dota_file('data.txt')
 print(f"Parsed {len(matches)} matches")
@@ -11,7 +8,7 @@ conn = load_matches_into_duckdb(matches)
 conn.sql("SELECT * FROM matches LIMIT 5").show()
 conn.sql("""
          SELECT m.match_id, m.date_time, p.player_name, p.kills
-         FROM players p JOIN matches m USING(match_id)
+         FROM players p
+                  JOIN matches m USING (match_id)
          ORDER BY p.kills DESC LIMIT 10
          """).show()
-
